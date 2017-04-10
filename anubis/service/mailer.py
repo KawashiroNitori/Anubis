748ac5e4698c24fadd56520d62c4b570376be7f2
Nitori <nitori@ence-dimension.com>
2017-04-11 00:20:22 +0800

    fix smtplib

--- anubis/service/mailer.py
+++ anubis/service/mailer.py
@@ -1,6 +1,6 @@
 from email.mime import text
 
-import aiosmtplib as asmtp
+import smtplib as asmtp
 
 from anubis.util import argmethod
 from anubis.util import options
@@ -9,21 +9,20 @@ options.define('smtp_host', default='smtp.qq.com', help='SMTP server')
 options.define('smtp_port', default=465, help='SMTP server port')
 options.define('smtp_user', default='acm_mail@ikazuchi.cn', help='SMTP username')
 options.define('smtp_password', default='emymknobimmlbbnb', help='SMTP password')
-options.define('mail_from', default='ACM Laboratory DevTeam of SUT', help='Mail from')
+options.define('mail_from', default='acm@sut.edu.cn', help='Mail from')
 
 
 @argmethod.wrap
 async def send_mail(to: str, subject: str, content: str):
-    return
     msg = text.MIMEText(content, _subtype='html', _charset='UTF-8')
     msg['Subject'] = subject
     msg['From'] = options.options.mail_from
     msg['To'] = to
 
-    async with asmtp.SMTP_SSL(hostname=options.options.smtp_host, port=options.options.smtp_port) as server:
-        await server.ehlo()
-        await server.login(options.options.smtp_user, options.options.smtp_password)
-        await server.sendmail(options.options.mail_from, to, msg.as_string())
+    with asmtp.SMTP_SSL(host=options.options.smtp_host, port=options.options.smtp_port) as server:
+        server.ehlo()
+        server.login(options.options.smtp_user, options.options.smtp_password)
+        server.sendmail(options.options.mail_from, to, msg.as_string())
 
 
 if __name__ == '__main__':
