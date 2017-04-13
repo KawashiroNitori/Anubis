67aa71c77696de8cc4c278e9192ad922b624d09f
Nitori <nitori@ence-dimension.com>
2017-04-13 16:14:44 +0800

    argument can change font size of pdf

--- anubis/service/gen_pdf.py
+++ anubis/service/gen_pdf.py
@@ -9,20 +9,21 @@ from reportlab.lib.styles import ParagraphStyle
 from anubis.util import options
 
 options.define('ttf_font_name', 'fzyh.ttf', 'Name of TTF Font')
+options.define('pdf_font_size', 84, 'Font Size of generated PDF.')
 
 A4_TRANSVERSE = A4[::-1]
 _reged = False
 
 
-def gen_team_pdf(team_tuples: list, fontsize: int=72):
+def gen_team_pdf(team_tuples: list):
     global _reged
     if not _reged:
         pdfmetrics.registerFont(TTFont('my_font', options.options.ttf_font_name))
         _reged = True
     buf = io.BytesIO()
     canv = canvas.Canvas(buf, pagesize=A4_TRANSVERSE)
-    style = ParagraphStyle(name='default', fontName='my_font', fontSize=fontsize,
-                           alignment=1, leading=fontsize * 1.2)
+    style = ParagraphStyle(name='default', fontName='my_font', fontSize=options.options.fontsize,
+                           alignment=1, leading=options.options.fontsize * 1.2)
     for index, team in team_tuples:
         paragraph = Paragraph('Team {0}<br />{1}'.format(index, team['team_name']), style)
         w, h = paragraph.wrap(A4_TRANSVERSE[0] - 100, A4_TRANSVERSE[1] - 100)
