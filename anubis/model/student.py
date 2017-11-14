962b11a0f2cc6250219cb95874eb80bc54d4ed3a
Coder-Zs <coderzs1314@gmail.com>
2017-11-14 14:55:38 +0800

    add 17 students info (#4)
    * Update student.py
    
    * Update student.py
    
    * collect 17 studnts info
    
    * Update student
    
    * Update student
    
    * Udpate student
    
    * Update student.py

--- anubis/model/student.py
+++ anubis/model/student.py
@@ -59,6 +59,19 @@ async def create_indexes():
     await coll.create_index('id_number', unique=True)
     await coll.create_index('class')
 
-
+async def main():
+    for s_id in range(170405101, 170405350):
+        if s_id % 100 >= 50:
+            continue
+        student = await get(str(s_id))
+    for s_id in range(171203101, 171203850):
+        if s_id % 100 >= 50:
+            continue
+        student = await get(str(s_id))
+    for s_id in range(171201101, 171201650):
+        if s_id % 100 >= 50:
+            continue
+        student = await get(str(s_id))
+        
 if __name__ == '__main__':
     argmethod.invoke_by_args()
