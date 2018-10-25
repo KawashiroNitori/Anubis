d6850487cdf1525b0f2b18ef7a3ea43f9c706de3
KawashroNitori <pandamonium@qq.com>
2018-10-26 02:10:19 +0800

    add env variable to options

--- anubis/util/options.py
+++ anubis/util/options.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import sys
 
 _parser = argparse.ArgumentParser()
@@ -9,11 +10,17 @@ leftovers = sys.argv[1:]
 def define(name: str, default=None, help=None):
     flag_name = '--' + name.replace('_', '-')
     flag_type = type(default)
+    env_name = name.upper()
+    env_value = os.environ.get(env_name)
     if flag_type is bool:
+        if env_value is not None:
+            default = True
         _parser.add_argument(flag_name, dest=name, action='store_true', help=help)
         _parser.add_argument('--no-' + name.replace('_', '-'), dest=name, action='store_false')
         _parser.set_defaults(**{name: default})
     else:
+        if env_value is not None:
+            default = flag_type(env_value)
         _parser.add_argument(flag_name, dest=name, default=default, type=flag_type, help=help)
     global leftovers
     _, leftovers = _parser.parse_known_args(args=sys.argv[1:], namespace=options)
