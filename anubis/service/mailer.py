f73e6c8c0c25051bf706181b855939d17b00a810
KawashroNitori <pandamonium@qq.com>
2018-10-26 00:18:47 +0800

    change mailer to SendCloud

--- anubis/service/mailer.py
+++ anubis/service/mailer.py
@@ -1,28 +1,28 @@
-from email.mime import text
-
-import smtplib as asmtp
+from aiohttp import ClientSession
 
 from anubis.util import argmethod
 from anubis.util import options
 
-options.define('smtp_host', default='smtp.qq.com', help='SMTP server')
-options.define('smtp_port', default=465, help='SMTP server port')
-options.define('smtp_user', default='acm_mail@ikazuchi.cn', help='SMTP username')
-options.define('smtp_password', default='emymknobimmlbbnb', help='SMTP password')
-options.define('mail_from', default='acm@sut.edu.cn', help='Mail from')
+options.define('mail_from', default='acm@sutacm.cn', help='Mail from')
+options.define('mail_api_user', default='api_user', help='SendCloud API User')
+options.define('mail_api_key', default='api_key', help='SendCloud API Key')
 
 
 @argmethod.wrap
 async def send_mail(to: str, subject: str, content: str):
-    _server = asmtp.SMTP_SSL(host=options.options.smtp_host, port=options.options.smtp_port)
-    msg = text.MIMEText(content, _subtype='html', _charset='UTF-8')
-    msg['Subject'] = subject
-    msg['From'] = options.options.mail_from
-    msg['To'] = to
+    data = {
+        'apiUser': options.options.mail_api_user,
+        'apiKey': options.options.mail_api_key,
+        'to': to,
+        'from': options.options.mail_from,
+        'fromName': 'ACM 实验室',
+        'subject': subject,
+        'html': content,
+    }
 
-    _server.ehlo()
-    _server.login(options.options.smtp_user, options.options.smtp_password)
-    _server.sendmail(options.options.mail_from, to, msg.as_string())
+    async with ClientSession() as session:
+        async with session.post('http://api.sendcloud.net/apiv2/mail/send', data=data) as response:
+            return await response.json()
 
 
 if __name__ == '__main__':
